a7c9b4a4f2c0388d71895d558af69a7d9411a859
devin <devin.hasler@student.ffhs.ch>
2021-11-21 23:02:16 +0100

    commit with kapitel 7 and kapitel8

--- src/jaf/kapitel8/pp83/PP83.java
+++ src/jaf/kapitel8/pp83/PP83.java
@@ -1,5 +1,39 @@
 package jaf.kapitel8.pp83;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class PP83 {
 
+  public static void main(String[] args) throws FileNotFoundException {
+
+    ArrayList<Integer> numbers = new ArrayList<>();
+
+    Scanner scan = new Scanner(new File("src/jaf/kapitel8/pp83/Numbers83.txt"));
+    String numberScan;
+    while (scan.hasNext()) {
+      numberScan = scan.nextLine();
+      int number = Integer.parseInt(String.valueOf(numberScan));
+      numbers.add(number);
+    }
+    scan.close();
+
+    int min = 1;
+    int max = 10;
+    for (int j = 1; j <= 10; j++) {
+      System.out.print(min + " - ");
+      System.out.printf("%-5d", max);
+      System.out.print(" | \t");
+      for (Integer number : numbers) {
+        if ((number >= min) && (number <= max)) {
+          System.out.print("*");
+        }
+      }
+      System.out.println();
+      min += 10;
+      max += 10;
+    }
+  }
 }
